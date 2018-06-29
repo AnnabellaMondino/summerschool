efb6a657f19aff4a21bcd65aba4f0bee7815822a
training055 Student <training055@sisu-login2.csc.fi>
2018-06-29 11:40:09 +0300

    Exercise 3 of OpenMP lectures

--- openmp/work-sharing/sum.c
+++ openmp/work-sharing/sum.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define NX 102400
+#define NX 8//102400
 
 int main(void)
 {
@@ -14,13 +14,37 @@ int main(void)
         vecB[i] = vecA[i] * vecA[i];
     }
 
+        printf("vecA =\t");
+	for (i=0; i<NX; i++){
+		printf("%lf\t", vecA[i]);
+	}
+	printf("\n");
+
+	printf("vecB =\t");
+        for (i=0; i<NX; i++){
+                printf("%lf\t", vecB[i]);
+        }
+        printf("\n");
+
     /* TODO:
      *   Implement here a parallelized version of vector addition,
      *   vecC = vecA + vecB
      */
+#pragma omp parallel for 
+	for (i=0; i<NX; i++){
+		vecC[i] = vecA[i] + vecB[i];
+	}
+
+        printf("vecC =\t");
+        for (i=0; i<NX; i++){
+                printf("%lf\t", vecC[i]);
+        }
+	printf("\n");
+
 
     sum = 0.0;
     /* Compute the check value */
+#pragma omp parallel for shared(vecC) private(i) reduction(+:sum)
     for (i = 0; i < NX; i++) {
         sum += vecC[i];
     }
