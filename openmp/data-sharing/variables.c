5c76f8f683ed7d63cdeedea5614cce01f7ddc8b2
training055 Student <training055@sisu-login2.csc.fi>
2018-06-29 11:14:41 +0300

    First exercises with OpenMP

--- openmp/data-sharing/variables.c
+++ openmp/data-sharing/variables.c
@@ -7,7 +7,10 @@ int main(void)
     /* TODO:
      *   Test the effect of different data sharing clauses here
      */
+        printf("Before entering to a parallelization with private(var1)\n var1=%d\tvar2=%d\n",var1,var2);
+#pragma omp parallel //private(var1)
     {
+        var1 = 20;
         printf("Region 1: var1=%i, var2=%i\n", var1, var2);
         var1++;
         var2++;
